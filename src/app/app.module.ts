6ffe85e4e3abe66087b11561732dc17add8c44d9
pedropbazzo <32115702+pedropbazzo@users.noreply.github.com>
2019-05-18 18:02:03 -0300

    internet banking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from ‘@angular/material/dialog’;
+
 
 @NgModule({
   declarations: [
@@ -11,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
